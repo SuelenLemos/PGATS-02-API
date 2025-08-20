e6374c79286542573d084a482377f051eee25ae1
suelen.santos <suelen.santos@digicade.com.br>
2025-08-20 20:54:27 -0300

    test e ajustes

---
+++ test/controller/transferController.test.js
@@ -0,0 +1,24 @@
+//bibliotecas
+const requester = require('supertest');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+//aplicação
+const app = require('../../app');
+
+//testes
+describe('Transfer Controller', () => {
+    describe('POST /transfers', () => {
+        it('Quando informo remetente e destinatario inexistente recebo 400',async()=>{
+            const resposta = await requester(app)
+                .post('/transfers')
+                .send({
+                   from: "Suelen",
+                    to: "Julio",
+                    amount: 100
+                })
+            expect(resposta.status).to.equal(404);
+
+        })
+    });
+});
