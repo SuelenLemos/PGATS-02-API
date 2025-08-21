12523a17cc08460053d064fa5a620aae8b310633
suelen.santos <suelen.santos@digicade.com.br>
2025-08-20 21:46:54 -0300

    ajuste404

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -9,7 +9,7 @@ const app = require('../../app');
 //testes
 describe('Transfer Controller', () => {
     describe('POST /transfers', () => {
-        it('Quando informo remetente e destinatario inexistente recebo 400',async()=>{
+        it('Quando informo remetente e destinatario inexistente recebo 404',async()=>{
             const resposta = await requester(app)
                 .post('/transfers')
                 .send({
